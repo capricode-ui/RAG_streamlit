72ea860cdc9066ce26d3ddafd04e7e2d9aa64d01
Samruddhi Borhade <141911760+samruddhi1902@users.noreply.github.com>
2025-02-18 11:11:41 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# import chromadb
-# chromadb.api.client.SharedSystemClient.clear_system_cache()
+import chromadb
+chromadb.api.client.SharedSystemClient.clear_system_cache()
 
 def preprocess_text(files, size, overlap):
     import os
